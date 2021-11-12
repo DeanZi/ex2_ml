4e3d2fc5f328994209127b02f34ad6d1e6e746f5
deanzion <deanzion1@gmail.com>
2021-11-12 14:20:54 +0200

    knn works

--- main.py
+++ main.py
@@ -3,41 +3,56 @@ import numpy as np
 
 
 class Model:
-  def __init__(self, data_x, data_y, test_data, k=None):
-    self.data_x = data_x
-    self.data_y = data_y
-    self.test_data = test_data
-    self.k = k
+    def __init__(self, data_x, data_y, test_data, k=None):
+        self.data_x = data_x
+        self.data_y = data_y
+        self.test_data = test_data
+        self.k = k
 
-  def train(self):
-    pass
+    def train(self):
+        pass
 
-  def predict(self):
-    pass
+    def predict(self):
+        pass
+
 
 class KNN(Model):
 
-  def predict(self):
-
-
-
-
-
+    def predict(self):
+        predictions = []
+        for flower in self.test_data:
+            distances = {}
+            for i, example in enumerate(self.data_x):
+                distances[i] = np.linalg.norm(flower - example)
+            sorted_distances = dict(sorted(distances.items(), key=lambda item: item[1]))
+            labels_of_neigbours = []
+            for i in range(self.k):
+                labels_of_neigbours.append(self.data_y[list(sorted_distances.keys())[i]])
+            predictions.append(max(set(labels_of_neigbours), key=labels_of_neigbours.count))
+        return predictions
 
 
 def receive_data(examples_file, examples_labels_file, test_data_file):
-  examples_x = np.loadtxt(examples_file, dtype=float)
-  examples_y = np.loadtxt(examples_labels_file, dtype=int)
-  test_data = np.loadtxt(test_data_file, dtype=float)
-  return examples_x, examples_y, test_data
+    tmp_data_examples_x = []
+    with open(examples_file) as tmp_file:
+        for line in tmp_file:
+            tmp_data_examples_x.append([list(map(float, x.split(','))) for x in line.split(' ')])
+
+    examples_x = np.array(tmp_data_examples_x)
+    examples_y = np.loadtxt(examples_labels_file, dtype=int)
+
+    tmp_data_test_x = []
+    with open(examples_file) as tmp_file:
+        for line in tmp_file:
+            tmp_data_test_x.append([list(map(float, x.split(','))) for x in line.split(' ')])
+
+    test_data = np.array(tmp_data_test_x)
+    return examples_x, examples_y, test_data
+
 
 if __name__ == '__main__':
-  data_x, data_y, test_data = receive_data(sys.argv[0], sys.argv[1], sys.argv[2])
-  output_file_name = sys.argv[3]
-  knn = KNN(data_x, data_y, test_data)
-
-
-
-
-
-
+    data_x, data_y, test_data = receive_data(sys.argv[1], sys.argv[2], sys.argv[3])
+    output_file_name = sys.argv[4]
+    knn = KNN(data_x, data_y, test_data, 3)
+    knn_test_predictions = knn.predict()
+    print(knn_test_predictions)
